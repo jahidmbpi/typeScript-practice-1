4c10f2e031142375bc34c0942107a03a6d629f49
Jahidul Islam <jahid.redmi10@gmail.com>
2025-05-01 02:18:04 +0600

    this is frist commit

---
+++ module1/src/1.4.ts
@@ -0,0 +1,15 @@
+// string data type
+let fristName: string = "jahid";
+// number data type
+let rooNumber: number = 101;
+// boolean data type
+let isAdmin: boolean = true;
+//  undefaind data type
+let X: undefined = undefined;
+// nul data type
+let Y: null = null;
+
+let d: string;
+d = 123;
+d = "jahid";
+d = true;
